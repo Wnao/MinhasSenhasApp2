40819e750adae69f1c5e682c02cf895ef3ecff70
Wnao <43458392+Wnao@users.noreply.github.com>
2020-04-01 20:13:35 -0300

    password v2.2 Exer

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/MainActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
         } else{
-            Toast.makeText(this, "Usuário oou senha incorretos!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Usuário ou senha estão incorretos!", Toast.LENGTH_SHORT).show();
         }
 
       //   Intent intent = new Intent(this, ListaSenhasActivity.class);
